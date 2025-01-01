38b9e7ec2f690dae302896fe2b8d95556d882af7
nurmuhamadas <nurmuhamad.a.13@gmail.com>
2025-01-01 07:22:09 +0700

    FEATURE: create sign up usecase

---
+++ src/app/use-cases/sign-up.ts
@@ -0,0 +1,52 @@
+import { ERROR } from "@/common/constants/errors"
+import InvariantError from "@/common/exceptions/invariant-error"
+import { UserEntity } from "@/domains/auth/entities/user-entity"
+import { AuthRepository } from "@/domains/auth/repositories/auth-repository"
+
+import { AuthTokenManager } from "../security/auth-token-manager"
+import { PasswordHash } from "../security/password-hash"
+
+export class SignUp {
+  constructor(
+    private authRepository: AuthRepository,
+    private passwordHash: PasswordHash,
+    private tokenManager: AuthTokenManager,
+  ) {}
+
+  async execute(user: UserEntity): Promise<UserEntity> {
+    const isUsernameAvailable =
+      await this.authRepository.validateUsernameAvailability(user.username)
+    if (!isUsernameAvailable) {
+      throw new InvariantError(ERROR.USERNAME_ALREADY_EXIST)
+    }
+
+    const existingUser = await this.authRepository.getUserByEmail(user.email)
+    if (existingUser) {
+      throw new InvariantError(ERROR.EMAIL_ALREADY_EXIST)
+    }
+
+    const hashedPassword = await this.passwordHash.hash(user.password)
+
+    const token = await this.tokenManager.generateVerificationToken(
+      user.email,
+      user.username,
+    )
+
+    const createdUser = await this.authRepository.createUser(
+      user.username,
+      user.email,
+      hashedPassword,
+      token,
+    )
+
+    const userEntity = new UserEntity(
+      createdUser.id,
+      createdUser.username,
+      createdUser.email,
+      createdUser.password,
+      createdUser.emailVerifiedAt,
+    )
+
+    return userEntity
+  }
+}
