08ef06b59ba181263971ac5ad4248d71ed9d4928
nurmuhamadas <nurmuhamad.a.13@gmail.com>
2025-01-07 10:57:04 +0700

    FEATURE: update sign-in process to retrieve user settings and profile by email

--- src/app/use-cases/auth/sign-in.ts
+++ src/app/use-cases/auth/sign-in.ts
@@ -35,7 +35,8 @@ export class SignIn {
     },
     userAgent: string,
   ): Promise<SignInStatus> {
-    const existingUser = await this.authRepository.getUserByEmail(user.email)
+    const existingUser =
+      await this.authRepository.getUserWithSettingAndProfileByEmail(user.email)
     if (!existingUser) {
       throw new InvariantError(ERROR.EMAIL_NOT_REGISTERED)
     }
