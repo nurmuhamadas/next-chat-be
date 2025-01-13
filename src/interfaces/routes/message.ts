680aa4708c8e7a22216c226827fa9b55d6def370
nurmuhamadas <nurmuhamad.a.13@gmail.com>
2025-01-13 12:50:54 +0700

    FEATURE: update message route to dynamically select message creation use case based on room type

--- src/interfaces/routes/message.ts
+++ src/interfaces/routes/message.ts
@@ -1,9 +1,12 @@
 import { zValidator } from "@hono/zod-validator"
 import { Hono } from "hono"
 
+import { CreateChannelMessage } from "@/app/use-cases/messages/create-channel-message"
+import { CreateGroupMessage } from "@/app/use-cases/messages/create-group-message"
 import { CreatePrivateMessage } from "@/app/use-cases/messages/create-private-message"
 import { successResponse } from "@/common/lib/utils"
 import { CreateMessageEntity } from "@/domains/messages/entities/create-message-entity"
+import { RoomType } from "@/domains/rooms/entities/enums"
 import { container } from "@/infrastuctures/container"
 
 import { createMessageSchema } from "../schemas/message-schema"
@@ -20,7 +23,13 @@ const messageRoute = new Hono()
 
       const session = c.get("userSession")
 
-      const createMessage = container.get(CreatePrivateMessage)
+      const createMessage =
+        form.roomType === RoomType.PRIVATE
+          ? container.get(CreatePrivateMessage)
+          : form.roomType === RoomType.GROUP
+            ? container.get(CreateGroupMessage)
+            : container.get(CreateChannelMessage)
+
       const result = await createMessage.execute(
         session,
         CreateMessageEntity.fromJSON({
