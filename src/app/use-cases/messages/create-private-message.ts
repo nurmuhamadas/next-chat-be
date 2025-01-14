6434b4467808fcefc457c896082a06bcd20442c1
nurmuhamadas <nurmuhamad.a.13@gmail.com>
2025-01-14 08:36:28 +0700

    FEATURE: enhance createPrivateMessage use case to manage private chat options and unread message counts

--- src/app/use-cases/messages/create-private-message.ts
+++ src/app/use-cases/messages/create-private-message.ts
@@ -8,7 +8,14 @@ import { AttachmentEntity } from "@/domains/messages/entities/attachment-entity"
 import { CreateMessageEntity } from "@/domains/messages/entities/create-message-entity"
 import { MessageEntity } from "@/domains/messages/entities/message-entity"
 import { MessageRepository } from "@/domains/messages/repositories/message-repository"
+import { UnreadMessageRepository } from "@/domains/messages/repositories/unread-message-repository"
+import { CreatePrivateChatEntity } from "@/domains/private-chat/entites/create-private-chat-entity"
+import { UpdatePrivateChatOptionEntity } from "@/domains/private-chat/entites/update-private-chat-option-entity"
+import { PrivateChatOptionRepository } from "@/domains/private-chat/repositories/private-chat-option-repository"
 import { PrivateChatRepository } from "@/domains/private-chat/repositories/private-chat-repository"
+import { CreateRoomEntity } from "@/domains/rooms/entities/create-room-entity"
+import { RoomType } from "@/domains/rooms/entities/enums"
+import { RoomRepository } from "@/domains/rooms/repositories/room-repository"
 import { StorageRepository } from "@/domains/storage/repositories/storage-repository"
 import { KEYS } from "@/infrastuctures/container/keys"
 
@@ -39,6 +46,12 @@ export class CreatePrivateMessage {
     private blockedUserRepository: BlockedUserRepository,
     @inject(KEYS.PrivateChatRepository)
     private privateChatRepository: PrivateChatRepository,
+    @inject(KEYS.PrivateChatOptionRepository)
+    private privateChatOptionRepository: PrivateChatOptionRepository,
+    @inject(KEYS.RoomRepository)
+    private roomRepository: RoomRepository,
+    @inject(KEYS.UnreadMessageRepository)
+    private unreadMessageRepository: UnreadMessageRepository,
   ) {}
 
   async execute(
@@ -46,6 +59,7 @@ export class CreatePrivateMessage {
     data: CreateMessageEntity,
     attachments: File[] = [],
   ): Promise<MessageEntity> {
+    // VALIDATION
     if (!data.userReceiverId) {
       throw new InvariantError(ERROR.INVALID_TYPE, ["roomType"])
     }
@@ -108,11 +122,119 @@ export class CreatePrivateMessage {
         data.originalMessageId,
         data.attachments,
       )
+
+      const isBlocked = await this.blockedUserRepository.getIsUserBlocked(
+        data.userReceiverId,
+        session.userId,
+      )
+
+      const privateChat = await this.privateChatRepository.getByUserIds(
+        session.userId,
+        data.userReceiverId,
+      )
+      if (!privateChat) {
+        // NEVER CONTACT BEFORE
+        const newPrivateChat = await this.privateChatRepository.create(
+          new CreatePrivateChatEntity(session.userId, data.userReceiverId),
+        )
+
+        const createdMessage = await this.messageRepository.createMessage(
+          session.userId,
+          message,
+          parentMessage ?? undefined,
+        )
+
+        await this.roomRepository.createRoom(
+          new CreateRoomEntity(
+            RoomType.PRIVATE,
+            session.userId,
+            0,
+            createdMessage.id,
+            newPrivateChat.id,
+          ),
+        )
+
+        if (!isBlocked) {
+          await this.roomRepository.createRoom(
+            new CreateRoomEntity(
+              RoomType.PRIVATE,
+              data.userReceiverId,
+              1,
+              createdMessage.id,
+              newPrivateChat.id,
+            ),
+          )
+        } else {
+          const option =
+            await this.privateChatOptionRepository.getPrivateChatOptionByUserId(
+              data.userReceiverId,
+              session.userId,
+            )
+          await this.privateChatOptionRepository.updateOrCreatePrivateChatOption(
+            option?.id ?? "",
+            newPrivateChat.id,
+            new UpdatePrivateChatOptionEntity(data.userReceiverId, false),
+          )
+        }
+
+        return createdMessage
+      }
+
+      // INTERACTED BEFORE
+
       const createdMessage = await this.messageRepository.createMessage(
         session.userId,
         message,
         parentMessage ?? undefined,
       )
+
+      await this.roomRepository.updateLastMessage(
+        session.userId,
+        createdMessage.id,
+      )
+      if (!isBlocked) {
+        await this.roomRepository.updateLastMessage(
+          data.userReceiverId,
+          createdMessage.id,
+        )
+      }
+
+      const senderOpt =
+        await this.privateChatOptionRepository.getPrivateChatOptionByUserId(
+          session.userId,
+          data.userReceiverId,
+        )
+      await this.privateChatOptionRepository.updateOrCreatePrivateChatOption(
+        senderOpt?.id ?? "",
+        privateChat.id,
+        new UpdatePrivateChatOptionEntity(data.userReceiverId, false),
+      )
+
+      if (createdMessage.sender.id !== data.userReceiverId) {
+        const receiverOption =
+          await this.privateChatOptionRepository.getPrivateChatOptionByUserId(
+            data.userReceiverId,
+            session.userId,
+          )
+        await this.privateChatOptionRepository.updateOrCreatePrivateChatOption(
+          receiverOption?.id ?? "",
+          privateChat.id,
+          new UpdatePrivateChatOptionEntity(data.userReceiverId, false),
+        )
+      }
+
+      if (createdMessage.sender.id !== data.userReceiverId && !isBlocked) {
+        const receiverRoom = await this.roomRepository.getRoomByActionId(
+          data.userReceiverId,
+          session.userId,
+        )
+        if (receiverRoom) {
+          await this.unreadMessageRepository.incrementUnreadMessageCount(
+            data.userReceiverId,
+            session.userId,
+          )
+        }
+      }
 
       return createdMessage
     } catch (e) {
